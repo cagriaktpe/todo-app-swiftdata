0d34161eda7d74b267da47f34663e7e0b9468691
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-10-12 17:37:51 +0300

    Update ContentView.swift
    Update list view

--- ToDoApp/ContentView.swift
+++ ToDoApp/ContentView.swift
@@ -5,29 +5,76 @@
 //  Created by Samet Cagri Aktepe on 12/10/2023.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    
     @State private var showCreate = false
+    @Query private var items: [ToDoItem]
 
     var body: some View {
         NavigationStack {
-            Text("Hello")
-                .toolbar {
-                    ToolbarItem {
-                        Button(action: {
-                            showCreate.toggle()
-                        }, label: {
-                            Label("Add Item", systemImage: "plus")
-                        })
+            List {
+                ForEach(items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            if item.isCritical {
+                                Image(systemName: "exclamationmark.3")
+                                    .symbolVariant(.fill)
+                                    .foregroundColor(.red)
+                                    .font(.largeTitle)
+                                    .bold()
+                            }
+
+                            Text(item.title)
+                                .font(.largeTitle)
+                                .bold()
+
+                            Text("\(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .shortened))")
+                                .font(.callout)
+                        }
+
+                        Spacer()
+
+                        Button {
+                        } label: {
+                            Image(systemName: "checkmark")
+                                .symbolVariant(.circle.fill)
+                                .foregroundStyle(item.isCompleted ? .green : .gray)
+                                .font(.largeTitle)
+                        }
+                        .buttonStyle(.plain)
+                    }
+                    .swipeActions(edge: .trailing, allowsFullSwipe: false) {
+                        Button {
+                            withAnimation {
+                                modelContext.delete(item)
+                            }
+                        } label: {
+                            Label("Delete", systemImage: "trash")
+                        }
+                        .tint(.red)
                     }
                 }
-                .sheet(isPresented: $showCreate, content: {
-                    NavigationStack {
-                        CreateToDoView()
-                    }
-                    .presentationDetents([.medium])
-                })
+            }
+            .navigationTitle("My ToDo List")
+            .toolbar {
+                ToolbarItem {
+                    Button(action: {
+                        showCreate.toggle()
+                    }, label: {
+                        Label("Add Item", systemImage: "plus")
+                    })
+                }
+            }
+            .sheet(isPresented: $showCreate, content: {
+                NavigationStack {
+                    CreateToDoView()
+                }
+                .presentationDetents([.medium])
+            })
         }
     }
 }
