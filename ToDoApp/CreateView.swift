a1510a1edd1e222e6143514256315cefd6f50067
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-10-12 15:11:41 +0300

    Create CreateView.swift

---
+++ ToDoApp/CreateView.swift
@@ -0,0 +1,29 @@
+//
+//  CreatView.swift
+//  ToDoApp
+//
+//  Created by Samet Cagri Aktepe on 12/10/2023.
+//
+
+import SwiftUI
+
+struct CreateView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        List {
+            TextField("Name", text: .constant(""))
+            DatePicker("Choose a date", selection: .constant(.now))
+            Toggle("Important?", isOn: .constant(false))
+            Button("Create") {
+                dismiss()
+            }
+        
+        }
+        .navigationTitle("Create ToDo")
+    }
+}
+
+#Preview {
+    CreateView()
+}
