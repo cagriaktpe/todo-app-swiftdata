86bc37a495e149d7423649795e09182d77ca10c1
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-10-12 15:04:17 +0300

    Create TodoItem.swift

---
+++ ToDoApp/ToDoItem.swift
@@ -0,0 +1,25 @@
+//
+//  ToDoItem.swift
+//  ToDoApp
+//
+//  Created by Samet Cagri Aktepe on 12/10/2023.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class ToDoItem {
+    var title: String
+    var timestamp: Date
+    var isCritical: Bool
+    var isCompleted: Bool
+    
+    init(title: String = "", timestamp: Date = .now, isCritical: Bool = false, isCompleted: Bool = false) {
+        self.title = title
+        self.timestamp = timestamp
+        self.isCritical = isCritical
+        self.isCompleted = isCompleted
+    }
+    
+}
