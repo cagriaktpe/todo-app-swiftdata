62d2fb37fbb7fbb574a652f6432f84dc5882129f
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-10-12 17:50:13 +0300

    Create UpdateToDoView.swift

---
+++ ToDoApp/UpdateToDoView.swift
@@ -0,0 +1,27 @@
+//
+//  UpdateToDoView.swift
+//  ToDoApp
+//
+//  Created by Samet Cagri Aktepe on 12/10/2023.
+//
+
+import SwiftUI
+import SwiftData
+
+struct UpdateToDoView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @Bindable var item: ToDoItem
+    
+    var body: some View {
+        List {
+            TextField("Name", text: $item.title)
+            DatePicker("Choose a date", selection: $item.timestamp)
+            Toggle("Important?", isOn: $item.isCritical)
+            Button("Update") {
+                dismiss()
+            }
+        }
+        .navigationTitle("Update ToDo")
+    }
+}
