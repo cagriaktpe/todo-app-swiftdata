2e5485ee5cb1a497386fa5e5907a3fca3068df1a
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-10-12 11:56:36 +0300

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Samet Cagri Aktepe on 12/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
