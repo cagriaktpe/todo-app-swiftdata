eefaca00d20b28c2f962ec7b5d27df0351dd1ea4
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-10-23 15:24:23 +0300

    Update feature for UpdateToDoView.swift

--- ToDoApp/Views/UpdateToDoView.swift
+++ ToDoApp/Views/UpdateToDoView.swift
@@ -5,6 +5,7 @@
 //  Created by Samet Cagri Aktepe on 12/10/2023.
 //
 
+import PhotosUI
 import SwiftData
 import SwiftUI
 
@@ -12,6 +13,8 @@ struct UpdateToDoView: View {
     @Environment(\.dismiss) var dismiss
 
     @Query private var categories: [Category]
+    
+    @State var selectedPhoto: PhotosPickerItem?
 
     @Bindable var item: ToDoItem
 
@@ -43,6 +46,34 @@ struct UpdateToDoView: View {
                     .pickerStyle(.inline)
                 }
             }
+            
+            Section {
+                if let imageData = item.image,
+                   let uiImage = UIImage(data: imageData) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(maxWidth: /*@START_MENU_TOKEN@*/ .infinity/*@END_MENU_TOKEN@*/, maxHeight: 300)
+                }
+
+                PhotosPicker(selection: $selectedPhoto,
+                             matching: .images,
+                             photoLibrary: .shared()) {
+                    Label("Add Image", systemImage: "photo")
+                }
+
+                if item.image != nil {
+                    Button(role: .destructive) {
+                        withAnimation {
+                            selectedPhoto = nil
+                            item.image = nil
+                        }
+                    } label: {
+                        Label("Remove Image", systemImage: "xmark")
+                            .foregroundColor(.red)
+                    }
+                }
+            }
 
             Section {
                 Button("Update") {
@@ -63,6 +94,11 @@ struct UpdateToDoView: View {
                 Button("Save") {
                     dismiss()
                 }
+            }
+        }
+        .task(id: selectedPhoto) {
+            if let data = try? await selectedPhoto?.loadTransferable(type: Data.self) {
+                item.image = data
             }
         }
     }
